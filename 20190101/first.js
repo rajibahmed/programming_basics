f7f99ee64adbc2c95f182d79827b4d6e34a72db9
Rajib Ahmed <rajib.ahmed@klarna.com>
2019-01-01 23:23:53 +0100

    Adding comments for code

--- 20190101/first.js
+++ 20190101/first.js
@@ -2,13 +2,22 @@
 // This is a comment line 
 // var is short for variable 
 // variable is a holder/bucket/storage place for assigning values
-var first_number = 1;
-var second_number = 2;
+var first_number = 5;
+var second_number = 10;
 
-var string_int = "ahmed"
+var total_of_two_number = first_number + second_number;
+console.log('This is a lable for the print', total_of_two_number);
 
+
+// Adding two string to create a longer string
 var first_name = "Riaze";
 var last_name = "Ahmed";
 
-var full_name =  12 + Number(string_int);
-console.log(full_name);
+var full_name = first_name + ' ' + last_name;
+console.log('Printing fullname', full_name);
+
+
+//here I am adding one number with a string 
+var string_int = "12"
+var sumation_of_number_and_forced_number =  12 + Number(string_int);
+console.log('susumation_of_number_and_forced_number', sumation_of_number_and_forced_number);
