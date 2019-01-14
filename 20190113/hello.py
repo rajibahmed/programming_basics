00995b296634ab6f7d42fb2b2dad51b6c009b367
Rajib Ahmed <rajib.ahmed@klarna.com>
2019-01-14 16:12:10 +0100

    Add third

---
+++ 20190113/hello.py
@@ -0,0 +1,15 @@
+# A = 80 plus 
+# B = 70 - 80
+# C = 60 - 70
+# F < 60
+# extraction logic
+
+grade = 60
+
+gotA = grade > 80 
+
+# logic test can be only used when out is boolen
+if gotA :
+    print("this student has grade A")
+else:
+    print("This studen has failed")
